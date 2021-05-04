cb16d6f70557e12fc4eddac0738fb3173ad349b8
allanCastro497 <kennedy.allan.ti@hotmail.com>
2021-05-03 21:16:24 -0300

    Commit inicial | Páginas hom e post interno feito de maneira estática ainda

---
+++ index.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const app = express();
+const bodyParser = require('body-parser');
+
+// Setando as configurações do body parser
+app.use(bodyParser.urlencoded({extended:false}))
+app.use(bodyParser.json())
+
+// Setando as configurações do EJS
+app.set('view engine', 'ejs')
+app.use(express.static('public'))
+
+app.get('/', (req,res) => {
+    let title = 'Blog | Página Inicial'
+    res.render('index.ejs', {title: title})
+})
+app.get('/diversos/post', (req, res) => {
+    let title = 'Post Example | Página Inicial'
+    res.render('post.ejs', {title: title})
+})
+
+
+app.listen(80, () => {
+    console.log('Servidor Rodando ...')
+})
